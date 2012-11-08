2ed2c8bb576e0371d1a686baee9e075764d4d0f8
PR Lambert <pr@prlambert.com>
2012-11-08 14:09:23 -0800

    Fixing typos and licensing details

--- commands/wysiwyg_create_link_command.js
+++ commands/wysiwyg_create_link_command.js
@@ -1,10 +1,10 @@
 // ==========================================================================
-// Project:   SproutCore WYSIWYG 
-// Copyright: C 2012 Matygo Education Incorporated operating as Learndot
+// Project:   SproutCore WYSIWYG
+// Copyright: ©2012 Matygo Educational Incorporated operating as Learndot
 // Author:    Joe Gaudet (joe@learndot.com) and contributors (see contributors.txt)
-// License:   Licensed under MIT license (see license.js) 
+// License:   Licensed under MIT license (see license.js)
 // ==========================================================================
-/*globals SproutCoreWysiwyg */
+// /*globals SproutCoreWysiwyg */
 sc_require('commands/wysiwyg_command');
 sc_require('commands/wysiwyg_picker_command_support');
 sc_require('panes/wysiwyg_link_picker_pane');
