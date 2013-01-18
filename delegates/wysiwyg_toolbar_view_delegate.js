821218caadcb713bbcbb3ab572ba95d2f9ccfd7e
Joe Gaudet <joe@learndot.com>
2013-01-17 16:42:33 -0800

    tweak

--- delegates/wysiwyg_toolbar_view_delegate.js
+++ delegates/wysiwyg_toolbar_view_delegate.js
@@ -9,67 +9,69 @@ sc_require('views/wysiwyg_select_view');
 
 /**
  * @class
- * 
+ *
  * Responsible for creating toolbar controls for command objects
  */
 SC.WYSIWYGToolbarViewDelegate = {
 
-	isWYSIWYGToolbarViewDelegate: YES,
+    isWYSIWYGToolbarViewDelegate: YES,
 
-	controller: null,
+    controller: null,
 
-	toolbarViewCreateControlForCommandNamed: function(toolbarView, commandName) {
-		var command = SC.WYSIWYGCommandFactory.commandFor(commandName);
-		var controlView = command ? this.toolbarViewButtonForCommand(toolbarView, commandName, command) : this[commandName];
-		if (controlView) {
-			controlView = this[commandName] = toolbarView.createChildView(controlView);
-			if (SC.ButtonView.hasSubclass(controlView.constructor)) {
-				controlView.adjust('height', SC.Theme.find(SC.defaultTheme).buttonRenderDelegate[SC.REGULAR_CONTROL_SIZE].height);
-			}
-		} else {
-			SC.error('WYSIWYGToolbarViewDelegate: Could not createView: ' + commandName + ' no class was found.');
-		}
-		return controlView;
-	},
+    toolbarViewCreateControlForCommandNamed: function (toolbarView, commandName) {
+        var command = SC.WYSIWYGCommandFactory.commandFor(commandName);
+        var controlView = command ? this.toolbarViewButtonForCommand(toolbarView, commandName, command) : this[commandName];
+        if (controlView) {
+            controlView = this[commandName] = toolbarView.createChildView(controlView);
+            if (SC.ButtonView.hasSubclass(controlView.constructor)) {
+                controlView.adjust('height', SC.Theme.find(SC.defaultTheme).buttonRenderDelegate[SC.REGULAR_CONTROL_SIZE].height);
+            }
+        }
+        else {
+            SC.error('WYSIWYGToolbarViewDelegate: Could not createView: ' + commandName + ' no class was found.');
+        }
+        return controlView;
+    },
 
-	toolbarViewButtonForCommand: function(toolbarView, key, command) {
-		var buttonClass = this[key];
-		if (buttonClass) {
-			buttonClass = buttonClass.extend({
-				command: command
-			});
-		} else {
-			buttonClass = this.get('exampleView').extend({
-				layout: {
-					width: 30,
-					height: SC.Theme.find(SC.defaultTheme).buttonRenderDelegate[SC.REGULAR_CONTROL_SIZE].height
-				},
-				icon: command.get('icon'),
-				command: command,
-				toolTip: command.get('toolTip'),
-				action: 'invokeCommand',
-				target: this,
-				keyEquivalent: command.get('keyEquivalent'),
-				isSelectedBinding: SC.Binding.oneWay('.parentView.controller.is' + command.commandName.classify())
-			});
-		}
-		return buttonClass;
-	},
+    toolbarViewButtonForCommand: function (toolbarView, key, command) {
+        var buttonClass = this[key];
+        if (buttonClass) {
+            buttonClass = buttonClass.extend({
+                command: command
+            });
+        }
+        else {
+            buttonClass = this.get('exampleView').extend({
+                layout: {
+                    width: 30,
+                    height: SC.Theme.find(SC.defaultTheme).buttonRenderDelegate[SC.REGULAR_CONTROL_SIZE].height
+                },
+                icon: command.get('icon'),
+                command: command,
+                toolTip: command.get('toolTip'),
+                action: 'invokeCommand',
+                target: this,
+                keyEquivalent: command.get('keyEquivalent'),
+                isSelectedBinding: SC.Binding.oneWay('.parentView.controller.is' + command.commandName.classify())
+            });
+        }
+        return buttonClass;
+    },
 
-	invokeCommand: function(source) {
-		this.get('controller').invokeCommand(source);
-	},
+    invokeCommand: function (source) {
+        this.get('controller').invokeCommand(source);
+    },
 
-	/**
-	 * @property {SC.WYSIWYGSelectView} default control for handling paragraph
-	 *           styles (p, h1, h2 etc)
+    /**
+     * @property {SC.WYSIWYGSelectView} default control for handling paragraph
+     *           styles (p, h1, h2 etc)
+     */
+    styles: SC.WYSIWYGSelectView,
+
+    /**
+     * @property {SC.ButtonView) default control for handling commands.
 	 */
-	styles: SC.WYSIWYGSelectView,
-
-	/**
-	 * @property {SC.ButtonView) default control for handling commands.
-	 */
-	exampleView: SC.ButtonView.extend({
-		classNames: 'sc-wysiwyg-button'
-	})
+    exampleView: SC.ButtonView.extend({
+        classNames: 'sc-wysiwyg-button'
+    })
 };
