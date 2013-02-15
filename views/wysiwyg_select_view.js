e4270c09938fdf0beda439c5010df28733aff053
Joe Gaudet <joe@learndot.com>
2013-02-14 21:20:47 -0800

    Resolves a bug where the wysiwyg editor styles drop down disappears

--- views/wysiwyg_select_view.js
+++ views/wysiwyg_select_view.js
@@ -7,76 +7,76 @@
 /*globals SproutCoreWysiwyg */
 
 SC.WYSIWYGSelectView = SC.PopupButtonView.extend({
-	titleBinding: SC.Binding.transform(function(value) {
-		value = SproutCoreWysiwyg.styles.findProperty('value', value);
-		if (!value) {
-			value = {
-				title: "Paragraph"
-			};
-		}
-		return value.title.replace(/<[^>]+>([^<]+)<[^>]+>/, '$1');
-	}).oneWay('.parentView.controller.currentStyle'),
+    titleBinding: SC.Binding.transform(function (value) {
+        value = SproutCoreWysiwyg.styles.findProperty('value', value);
+        if (!value) {
+            value = {
+                title: "Paragraph"
+            };
+        }
+        return value.title.replace(/<[^>]+>([^<]+)<[^>]+>/, '$1');
+    }).oneWay('.parentView.controller.currentStyle'),
 
-	layout: {
-		width: 120,
-		height: SC.Theme.find(SC.defaultTheme).buttonRenderDelegate[SC.REGULAR_CONTROL_SIZE].height
-	},
+    layout: {
+        width: 120,
+        height: SC.Theme.find(SC.defaultTheme).buttonRenderDelegate[SC.REGULAR_CONTROL_SIZE].height
+    },
 
-	_instantiateMenu: function() {
-		var menu = this.get('menu');
-		if (!menu || !menu.isClass) return;
-		this.menu = menu.create({
-			button: this
-		});
-		this._setupMenu();
-	},
+    _instantiateMenu: function () {
+        var menu = this.get('menu');
+        if (!menu || !menu.isClass) return;
+        this.menu = menu.create({
+            button: this
+        });
+        this._setupMenu();
+    },
 
-	formatBlock: function(source) {
-		this.command.set('argument', '<%@>'.fmt(source.selectedItem.value.toUpperCase()));
-		var controller = this.getPath('parentView.controller');
-		if (controller) controller.invokeCommand(this);
-	},
+    formatBlock: function (source) {
+        this.command.set('argument', '<%@>'.fmt(source.selectedItem.value.toUpperCase()));
+        var controller = this.getPath('parentView.controller');
+        if (controller) controller.invokeCommand(this);
+    },
 
-	menu: SC.MenuPane.extend({
-		layout: {
-			width: SC.browser.isMac ? 240 : 260
-		},
-		menuHeightPadding: 0,
-		items: function() {
-			var button = this.button;
-			return SproutCoreWysiwyg.styles.map(function(values) {
-				return SC.Object.create(values, {
-					target: button,
-					shortcut: function() {
-						var keyEquivalent = this.get('keyEquivalent');
-						if (keyEquivalent) {
-							if (SC.browser.isMac) {
-								keyEquivalent = keyEquivalent.replace('ctrl_', '⌘');
-								keyEquivalent = keyEquivalent.replace('shift_', '⇧');
-								keyEquivalent = keyEquivalent.replace('alt_', '⌥');
-							}
-							else {
-								keyEquivalent = keyEquivalent.replace('ctrl_', 'Ctrl+');
-								keyEquivalent = keyEquivalent.replace('shift_', 'Shift+');
-								keyEquivalent = keyEquivalent.replace('alt_', 'Alt+');
-							}
-						}
-						return keyEquivalent;
-					}.property()
-				});
-			});
-		}.property().cacheable(),
+    menu: SC.MenuPane.extend({
+        layout: {
+            width: SC.browser.isMac ? 240 : 260
+        },
+        menuHeightPadding: 0,
+        items: function () {
+            var button = this.button;
+            return SproutCoreWysiwyg.styles.map(function (values) {
+                return SC.Object.create(values, {
+                    target: button,
+                    shortcut: function () {
+                        var keyEquivalent = this.get('keyEquivalent');
+                        if (keyEquivalent) {
+                            if (SC.browser.isMac) {
+                                keyEquivalent = keyEquivalent.replace('ctrl_', '⌘');
+                                keyEquivalent = keyEquivalent.replace('shift_', '⇧');
+                                keyEquivalent = keyEquivalent.replace('alt_', '⌥');
+                            }
+                            else {
+                                keyEquivalent = keyEquivalent.replace('ctrl_', 'Ctrl+');
+                                keyEquivalent = keyEquivalent.replace('shift_', 'Shift+');
+                                keyEquivalent = keyEquivalent.replace('alt_', 'Alt+');
+                            }
+                        }
+                        return keyEquivalent;
+                    }.property()
+                });
+            });
+        }.property().cacheable(),
 
-		exampleView: SC.MenuItemView.extend({
-			classNames: 'sc-wysiwyg-menu-item',
-			escapeHTML: NO,
-			render: function(context) {
-				sc_super();
-				context.addStyle({
-					lineHeight: this.get('layout').height
-				});
-			}
+        exampleView: SC.MenuItemView.extend({
+            classNames: 'sc-wysiwyg-menu-item',
+            escapeHTML: NO,
+            render: function (context) {
+                sc_super();
+                context.addStyle({
+                    lineHeight: this.get('layout').height + 'px'
+                });
+            }
 
-		})
-	})
+        })
+    })
 });
