f55ba9af025e54f518de53e0dded6ad3bde4b412
Nicolas BADIA <badia.nicolas@gmail.com>
2014-06-03 16:49:10 +0200

    Adds french translations

---
+++ french.lproj/strings.js
@@ -0,0 +1,38 @@
+/*-------------------------------------------------------------------------------------------------
+ - Project:   Sproutcore WYSIWYG                                                                  -
+ - Copyright: ©2012 Matygo Educational Incorporated operating as Learndot                         -
+ - Author:    Joe Gaudet (joe@learndot.com) and contributors (see contributors.txt)               -
+ - License:   Licensed under MIT license (see license.js)                                         -
+ -------------------------------------------------------------------------------------------------*/
+
+SC.stringsFor('fr', {
+  'Paragraph': 'Paragraphe',  
+  '<h1>Heading 1</h1>': '<h1>Titre 1</h1>',
+  '<h2>Heading 2</h2>': '<h2>Titre 2</h2>',
+  '<h3>Heading 3</h3>': '<h3>Titre 3</h3>',
+  '<h4>Heading 4</h4>': '<h4>Titre 4</h4>',
+  '<h5>Heading 5</h5>': '<h5>Titre 5</h5>',
+  '<h6>Heading 6</h6>': '<h6>Titre 6</h6>',
+  
+  'Bold text': 'Gras',  
+  'Italicize text': 'Italique',
+  'Underline text': 'Souligné', 
+  'Insert an ordered list': 'Liste ordonnée', 
+  'Insert an unordered list': 'Liste non ordonnée', 
+  'Left justify text': 'Aligner le texte à gauche', 
+  'Center justify text': 'Centrer le texte', 
+  'Right justify text': 'Agliner le texte à droite', 
+  'Justify text': 'justifer le texte', 
+  'Indent text': 'Réduire le retrait', 
+  'Outdent text': 'Augmenter le retrait', 
+  'Format text': 'Format du texte', 
+  'Insert a link': 'Insérer un lien',
+  'Insert Image': 'Insérer une image', 
+  'Remove format': 'Supprimer le formatage', 
+  'Redo': 'Rétablir', 
+  'Redo': 'Rétablir', 
+  'Undo': 'Annuler', 
+  'Redo': 'Rétablir', 
+  
+  'Clean': 'Nettoyer',
+});
