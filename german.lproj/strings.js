3be693c07167798a0fa5edb7b1589c56d2af0c6a
Nicolas BADIA <nicolas@gestixi.com>
2023-03-01 03:01:29 +0100

    Add translations in Spanish and German

---
+++ german.lproj/strings.js
@@ -0,0 +1,35 @@
+/*-------------------------------------------------------------------------------------------------
+ - Project:   Sproutcore WYSIWYG                                                                  -
+ - Copyright: ©2012 Matygo Educational Incorporated operating as Learndot                         -
+ - Author:    Joe Gaudet (joe@learndot.com) and contributors (see contributors.txt)               -
+ - License:   Licensed under MIT license (see license.js)                                         -
+ -------------------------------------------------------------------------------------------------*/
+
+ SC.stringsFor('de', {
+  'Paragraph': 'Absatz',
+  '<h1>Heading 1</h1>': '<h1>Überschrift 1</h1>',
+  '<h2>Heading 2</h2>': '<h2>Überschrift 2</h2>',
+  '<h3>Heading 3</h3>': '<h3>Überschrift 3</h3>',
+  '<h4>Heading 4</h4>': '<h4>Überschrift 4</h4>',
+  '<h5>Heading 5</h5>': '<h5>Überschrift 5</h5>',
+  '<h6>Heading 6</h6>': '<h6>Überschrift 6</h6>',
+
+  'Bold text': 'Fettdruck',
+  'Italicize text': 'Text kursiv',
+  'Underline text': 'Text unterstreichen',
+  'Insert an ordered list': 'Geordnete Liste einfügen',
+  'Insert an unordered list': 'Eine ungeordnete Liste einfügen',
+  'Left justify text': 'Text links ausrichten',
+  'Center justify text': 'Text zentrieren',
+  'Right justify text': 'Text rechts ausrichten',
+  'Justify text': 'Text ausrichten',
+  'Indent text': 'Text einrücken',
+  'Outdent text': 'Text ausrücken',
+  'Format text': 'Text formatieren',
+  'Insert a link': 'Link einfügen',
+  'Insert Image': 'Bild einfügen',
+  'Remove format': 'Format entfernen',
+  'Redo': 'Wiederholen',
+  'Undo': 'Rückgängig',
+  'Clean': 'Sauber',
+});
---
+++ spanish.lproj/strings.js
@@ -0,0 +1,35 @@
+/*-------------------------------------------------------------------------------------------------
+ - Project:   Sproutcore WYSIWYG                                                                  -
+ - Copyright: ©2012 Matygo Educational Incorporated operating as Learndot                         -
+ - Author:    Joe Gaudet (joe@learndot.com) and contributors (see contributors.txt)               -
+ - License:   Licensed under MIT license (see license.js)                                         -
+ -------------------------------------------------------------------------------------------------*/
+
+ SC.stringsFor('es', {
+  'Paragraph': 'Párrafo',
+  '<h1>Heading 1</h1>': '<h1>Título 1</h1>',
+  '<h2>Heading 2</h2>': '<h2>Título 2</h2>',
+  '<h3>Heading 3</h3>': '<h3>Título 3</h3>',
+  '<h4>Heading 4</h4>': '<h4>Título 4</h4>',
+  '<h5>Heading 5</h5>': '<h5>Título 5</h5>',
+  '<h6>Heading 6</h6>': '<h6>Título 6</h6>',
+
+  'Bold text': 'Negrita',
+  'Italicize text': 'En cursiva',
+  'Underline text': 'Subrayar',
+  'Insert an ordered list': 'Insertar una lista ordenada',
+  'Insert an unordered list': 'Insertar una lista desordenada',
+  'Left justify text': 'Texto justificado a la izquierda',
+  'Center justify text': 'Centrar texto justificado',
+  'Right justify text': 'Texto justificado a la derecha',
+  'Justify text': 'Justificar texto',
+  'Indent text': 'Reducir sangría',
+  'Outdent text': 'Aumentar outdent',
+  'Format text': 'Formato de texto',
+  'Insert a link': 'Insertar un enlace',
+  'Insert Image': 'Insertar Imagen',
+  'Remove format': 'Eliminar formato',
+  'Redo': 'Rehacer',
+  'Undo': 'Deshacer',
+  'Clean': 'Limpiar',
+});
